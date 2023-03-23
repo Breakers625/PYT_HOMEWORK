4b7ac271264e76ecca0a2be3ec573ed7109217d2
unknown <cass.25342@gmail.com>
2023-03-23 17:41:39 +0800

    variable solutions final

---
+++ Solutions_Final.py
@@ -0,0 +1,67 @@
+#variable solutions
+#2 beeps, 6 boops H
+beeps = 2
+boops = 6
+total = beeps + boops
+print(total)
+#0 beeps, 5 boops E
+beeps = 0
+boops = 5
+total = beeps + boops
+print(total)
+#9 beeps, 3 boops L
+beeps = 9
+boops = 3
+total = beeps + boops
+print(total)
+#4 beeps, 8 boops L
+beeps = 4
+boops = 8
+total = beeps + boops
+print(total)
+#10 beeps, 5 boops O
+beeps = 10
+boops = 5
+total = beeps + boops
+print(total)
+#BOP! (pretty sure this is a space!)
+Space = " "
+print(Space)
+#11 beeps, 12 boops W
+beeps = 11
+boops = 12
+total = beeps + boops
+print(total)
+#5 beeps, 5 boops J
+beeps = 5
+boops = 5
+total = beeps + boops
+print(total)
+#1 beep, 17 boops R
+beeps = 1
+boops = 17
+total = beeps + boops
+print(total)
+#5 beeps, 7 boops L
+beeps = 5
+boops = 7
+total = beeps + boops
+print(total)
+#4 beeps, 0 boops D
+beeps = 4
+boops = 0
+total = beeps + boops
+print(total)
+#output from anaconda
+#(base) C:\Users\snowf\OneDrive\Desktop>python Solutions_Final.py
+#8 = H
+#5 = E
+#12 = L
+#12 = L
+#15 = O
+
+#23 = W
+#10 = J
+#18 = R
+#12 = L
+#4 = D
