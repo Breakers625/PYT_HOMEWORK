030d6ca120cf46ce3f6e1aa6ddd048d3a837d1f9
unknown <cass.25342@gmail.com>
2023-03-30 23:51:03 +0800

    code_for_problem1

---
+++ problem1.py
@@ -0,0 +1,12 @@
+t = 14  # time of the day
+
+if t < 9:
+    print("Morning is wonderful. Its only drawback is that it comes at such an inconvenient time of day.")
+elif t <= 16:
+    print("Working hard or hardly working?")
+elif t < 20:
+    print("How did it get so late so soon?")
+elif t < 22:
+    print("A day without sunshine is like, you know, night.")
+else:
+    print("Burning the midnight oil!")
