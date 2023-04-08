69a9c63fe0a5be748268dde0cbb4b212478920ba
unknown <cass.25342@gmail.com>
2023-04-08 18:13:38 +0800

    air_con solution

---
+++ for_problem2.py
@@ -0,0 +1,8 @@
+temperature = 90
+desired_temperature = 75
+
+while temperature > desired_temperature:
+    print(f"The temperature is {temperature} - crank the AC")
+    temperature = temperature - 3
+
+print("\n75. Ahh, that's better.")
